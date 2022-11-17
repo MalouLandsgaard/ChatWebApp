5520f2cb8fb5b2911a47a8dcdcb2a83dc6bd6d12
MalouLandsgaard <elmelund@hotmail.dk>
2022-11-17 11:17:19 +0100

    with socket

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
-import express from "express";
-import path from "path";
+const app = require('express')
+const http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const PORT = 3001;
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+    console.log('new client connected');
+});
 
-app.get("api", (req, res) => {res.json({message: "Hello World!"})});
+// const express = require("express");
+// const path = require("path");
 
-app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));});
+// const PORT = process.env.PORT || 3001;
+// const app = express();
 
-app.listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
+// //retrieve static files
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+// //random paths redirect
+// app.use((req, res, next) => {res.redirect("/");});
+// //app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));});
+
+// app.get("/api", (req, res) => {res.json({message: 'Hello World!'})});
+
+// app.listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
 
